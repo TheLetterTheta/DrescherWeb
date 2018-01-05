8a62863d993d172a84460caa1577f96edf13d271
Nick Dolan <nicholas.dolan1@gmail.com>
2018-01-04 22:17:42 -0600

    feat(front-page): Updated UI According to Mr. Drescher's Wishes Includes LARGE icon and dummy text beneath for mailto tag.

--- drescher-web/src/app/app.module.ts
+++ drescher-web/src/app/app.module.ts
@@ -1,20 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './modules/app-routing.module';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NativeDateModule} from "@angular/material";
+import {AppMaterialModule} from "./modules/app-material.module";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    NativeDateModule,
+    AppRoutingModule,
+    AppMaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
